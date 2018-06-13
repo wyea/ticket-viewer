9c1ce9b2c35b0102061b892e58d8bf28faee3b69
wyea <wyea@yandex.com>
2018-06-13 03:09:18 -0700

    replace with double quotes

--- lib/ticket_presenter.rb
+++ lib/ticket_presenter.rb
@@ -10,7 +10,7 @@ class TicketPresenter
 
   def fetch_info(uri)
     request = Net::HTTP::Get.new(uri)
-    request.basic_auth(ENV['EMAIL_ADDRESS'], ENV['PASSWORD'])
+    request.basic_auth(ENV["EMAIL_ADDRESS"], ENV["PASSWORD"])
     Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
       http.request(request)
     end
