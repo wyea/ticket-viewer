bcdcc186fb4004aeb867818cda69d6e55ed50014
wyea <wyea@yandex.com>
2018-06-16 16:21:24 -0700

    introduce pagination, previous and next page methods

--- lib/ticket_presenter.rb
+++ lib/ticket_presenter.rb
@@ -5,6 +5,16 @@ require "json"
 require "time"
 
 class TicketPresenter
+  attr_reader :multipage_mode
+  attr_reader :number_of_pages
+
+  def initialize
+    @multipage_mode  = false
+    @previous_page   = ""
+    @next_page       = ""
+    @number_of_pages = 0
+  end
+
   def ticket_uri(ticket_number)
     URI("https://anar.zendesk.com/api/v2/tickets/#{ticket_number}.json")
   end
@@ -31,7 +41,7 @@ class TicketPresenter
   end
 
   def build_ticket_list(hash)
-    if hash["tickets"]
+    if hash["tickets"] && hash["tickets"].any?
       ticket_list = hash["tickets"].reduce([]) do |list, ticket|
         list.push(
           ticket["id"].to_s.rjust(4) + " | " +
@@ -40,10 +50,27 @@ class TicketPresenter
           ticket["subject"].to_s.ljust(68) + "\n"
         )
       end
+      paginate(hash)
       ticket_list_header + ticket_list.join("")
     else
-      "That server doesn't know anything about tickets... :-("
+      "The server doesn't know anything about "\
+        "the page you want to see... :-("
     end
+  end
+
+  def paginate(hash)
+    @multipage_mode  =  true if hash["next_page"]
+    @next_page       =  hash["next_page"] || ""
+    @previous_page   =  hash["previous_page"] || ""
+    @number_of_pages = (hash["count"].to_f / 25).ceil
+  end
+
+  def next_page
+    view_ticket_list(@next_page) unless @next_page.empty?
+  end
+
+  def previous_page
+    view_ticket_list(@previous_page) unless @previous_page.empty?
   end
 
   def ticket_list_header
